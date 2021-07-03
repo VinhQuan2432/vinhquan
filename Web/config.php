6d38761ddfb99443146959f60a159b2467bfad51
VinhQuan2432 <77775668+VinhQuan2432@users.noreply.github.com>
2021-07-03 19:00:38 +0700

    Update config.php

--- Web/config.php
+++ Web/config.php
@@ -1,9 +1,9 @@
 <?php
-$conn_string = "host = ec2-54-211-176-156.compute-1.amazonaws.com"
+$conn_string = "host = ec2-52-86-25-51.compute-1.amazonaws.com"
         . " port = 5432"
-        . " dbname = d7k35e83nt54kf"
-        . " user = lkblfujpnveoph"
-        . " password = a2dda41d2a64a9bc1de4ccc447742ff9c32185457810c259326670ce98c0fc42";
+        . " dbname = d2vjh5dkkg2ho4"
+        . " user = rokroxwldxzfaq"
+        . " password = 1bf3535343aec4ecd4972a67c492fcc1bcd8c8bc81805436fa5b827ffd2086b1";
 $db = pg_connect($conn_string);
 //connect to a database named "test" on the host "sheep" with a username and password
    if(!$db) {
